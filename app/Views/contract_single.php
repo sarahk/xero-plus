0a74ff46b8777b5224bbedf496e10a5ab8ac2d75
Sarah King <rainchx@gmail.com>
2024-11-10 12:41:35 +1300

    Remove log files from tracking

---
+++ app/Views/contract_single.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Views;
+
+//create some variables for the widgets
+$newNote = ['parent' => 'contact', 'foreign_id' => $data['contact']['id']];
+//var_dump($data);
+?>
+    <script>
+        const keys = <?=  json_encode($keys, JSON_PRETTY_PRINT) ?>;
+    </script>
+
+    <!-- PAGE-HEADER -->
+    <div class="page-header">
+        <div>
+            <h1 class="page-title">Invoice: <strong><?= $data['invoices']['invoice_number']; ?></strong></h1>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/authorizedResource.php?action=9">Invoices</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Invoice
+                    #<?= $data['invoices']['invoice_number']; ?></li>
+            </ol>
+        </div>
+    </div>
+    <!-- PAGE-HEADER END -->
+
+    <div class="row">
+        <div class="col-md-6 ">
+            <?php include 'Views/Widgets/combo-single-card.php' ?>
+            <?php include 'Views/Widgets/contact-card.php' ?>
+
+
+        </div>
+        <div class="col-md-6">
+            <?php include 'Views/Widgets/contract-card.php' ?>
+            <?php include 'Views/Widgets/notes-card.php' ?>
+        </div>
+    </div>
+    <?php
+include 'Widgets/combo-contact.php';
